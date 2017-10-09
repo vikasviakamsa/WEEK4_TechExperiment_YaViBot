22ba4c0989a37086f6c7ab3eda3c72220fca66af
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-09 19:16:34 -0400

    1_9

--- bot.rb
+++ bot.rb
@@ -37,11 +37,6 @@ message.reply(
 end
 
 Bot.on :postback do |postback|
-  postback.sender    # => { 'id' => '1008372609250235' }
-  postback.recipient # => { 'id' => '2015573629214912' }
-  postback.sent_at   # => 2016-04-22 21:30:36 +0200
-  postback.payload   # => 'EXTERMINATE'
-
   if postback.title == 'No'
     puts "you are marked for extermination!"
   end
