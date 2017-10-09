cac3131170588e4d22e72b541c435e7a000520ea
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-09 17:37:10 -0400

    1_1

--- bot.rb
+++ bot.rb
@@ -6,6 +6,29 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 
+Facebook::Messenger::Thread.set({
+      setting_type: 'call_to_actions',
+      thread_state: 'existing_thread',
+      call_to_actions: [
+        {
+          type: 'postback',
+          title: 'Coordinates lookup',
+          payload: 'COORDINATES'
+        },
+        {
+          type: 'postback',
+          title: 'Postal address lookup',
+          payload: 'FULL_ADDRESS'
+        },
+        {
+          type: 'postback',
+          title: 'Location lookup',
+          payload: 'LOCATION'
+        }
+      ]
+    }, access_token: ENV['ACCESS_TOKEN'])
+
+
 Bot.on :message do |message|
   puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
   message.reply(text: 'Hello!')
