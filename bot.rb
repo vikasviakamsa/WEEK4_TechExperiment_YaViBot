57cfc3e1baa4e0af411a08bb75fd2be9b14b1617
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-09 17:54:06 -0400

    1_4

--- bot.rb
+++ bot.rb
@@ -3,31 +3,7 @@ include Facebook::Messenger
 # NOTE: ENV variables should be set directly in terminal for testing on localhost
 
 # Subcribe bot to your page
-#Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
-
-  Facebook::Messenger::Thread.set({
-      setting_type: 'call_to_actions',
-      thread_state: 'existing_thread',
-      call_to_actions: [
-        {
-          type: 'postback',
-          title: 'Coordinates lookup',
-          payload: 'COORDINATES'
-        },
-        {
-          type: 'postback',
-          title: 'Postal address lookup',
-          payload: 'FULL_ADDRESS'
-        },
-        {
-          type: 'postback',
-          title: 'Location lookup',
-          payload: 'LOCATION'
-        }
-      ]
-    }, access_token: ENV['ACCESS_TOKEN'])
-
-
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
   puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
