c8315a360e68f7e33f2c226887cea1bdf6c54fd0
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-10 17:35:01 -0400

    2_2

--- bot.rb
+++ bot.rb
@@ -3,8 +3,7 @@ include Facebook::Messenger
 # NOTE: ENV variables should be set directly in terminal for testing on localhost
 
 # Subcribe bot to your page
-#
-#Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 #
 #Bot.on :message do |message|
 #  puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
