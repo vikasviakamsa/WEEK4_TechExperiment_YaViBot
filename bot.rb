f633fd8fee3920bbe21d1e7d33e190c356e40b71
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-10 17:38:50 -0400

    2_3

--- bot.rb
+++ bot.rb
@@ -1,50 +1,30 @@
-    require 'facebook/messenger'
+require 'facebook/messenger'
 include Facebook::Messenger
+
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
+
+
+
+
+
+
+
 # NOTE: ENV variables should be set directly in terminal for testing on localhost
 
 # Subcribe bot to your page
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+#Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 #
 #Bot.on :message do |message|
 #  puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
 #  message.reply(text: 'Hello!')
 #end
 
-Facebook::Messenger::Profile.set({
-  greeting: [
-    {
-      locale: 'default',
-      text: 'Welcome to your new bot overlord!'
-    },
-    {
-      locale: 'fr_FR',
-      text: 'Bienvenue dans le bot du Wagon !'
-    }
-  ]
-}, access_token: ENV['ACCESS_TOKEN'])
 
 
-
-
-Bot.on :message do |message|
-
-message.reply(
-  attachment: {
-    type: 'template',
-    payload: {
-      template_type: 'button',
-      text: 'Human, do you like me?',
-      buttons: [
-        { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
-        { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
-      ]
-    }
-  }
-)
-end
-
-Bot.on :postback do |postback|
-  if postback.title == 'No'
-    puts "you are marked for extermination!"
-  end
-end
+#Bot.on :postback do |postback|
+#  if postback.title == 'No'
+#    puts "you are marked for extermination!"
+#  end
+#end
